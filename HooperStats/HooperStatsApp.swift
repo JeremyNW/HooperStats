f693bb869c3ed0f7554ad9de98eb8a33edfd425b
JeremyNW <jeddynwarren@gmail.com>
2022-02-22 10:12:06 -0700

    Initial Commit

---
+++ HooperStats/HooperStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HooperStatsApp.swift
+//  HooperStats
+//
+//  Created by Jeremy Warren on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct HooperStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
