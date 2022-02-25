8d159211550d871ed407646ed0ee40b4718c9fc9
Zachary Buffington <88993567+Zbcolgrim@users.noreply.github.com>
2022-02-25 11:21:05 -0700

    Zachs branch (#4)
    * StatsView
    
    * StatsView
    
    * StatsView
    
    * Yo
    
    * Create .gitignore
    
    * Camer & PhotoPicker
    
    * remove user data
    
    * Delete Camer.swift
    
    Co-authored-by: JarWarren <43893126+JarWarren@users.noreply.github.com>

---
+++ HooperStats/Views/Camera.swift
@@ -0,0 +1,47 @@
+//
+//  Camera.swift
+//  HooperStats
+//
+//  Created by Zachary Buffington on 2/25/22.
+//
+
+import SwiftUI
+
+struct Camera: UIViewControllerRepresentable {
+    @Environment(\.dismiss) var dismiss
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        return picker
+    }
+
+    func updateUIViewController(_: UIImagePickerController, context _: Context) {
+        // no need to update
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: Camera
+        init(_ parent: Camera) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.image = image
+            }
+            parent.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_: UIImagePickerController) {
+            parent.dismiss()
+        }
+    }
+}
+
---
+++ HooperStats/Views/PhotoPicker.swift
@@ -0,0 +1,50 @@
+//
+//  PhotoPicker.swift
+//  HooperStats
+//
+//  Created by Zachary Buffington on 2/25/22.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct PhotoPicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.dismiss) var dismiss
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        let picker = PHPickerViewController(configuration: .init())
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_: PHPickerViewController, context _: Context) {
+        // nothing to update
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: PHPickerViewControllerDelegate {
+        let parent: PhotoPicker
+
+        init(_ parent: PhotoPicker) {
+            self.parent = parent
+        }
+
+        func picker(_: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            results.first?.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { object, error in
+                if let error = error {
+                    print(error)
+                }
+                if let image = object as? UIImage {
+                    DispatchQueue.main.async {
+                        self.parent.image = image
+                    }
+                }
+            })
+            parent.dismiss()
+        }
+    }
+}
