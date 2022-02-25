788085f7499330cf401d68ab708205f0e41d772c
Zbcolgrim <Zachary.n.buffington@gmail.com>
2022-02-25 10:59:47 -0700

    Camer & PhotoPicker

--- HooperStats/Views/StatsView.swift
+++ HooperStats/Views/StatsView.swift
@@ -8,22 +8,67 @@
 import SwiftUI
 
 struct StatsView: View {
+    @StateObject var model = StatsViewModel()
     var body: some View {
-        VStack {
-            Text("James Smith")
-                .padding()
-            VStack(alignment: .leading) {
-                Image(systemName: "person.circle")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
+        NavigationView {
+            VStack {
+                Text("James Smith")
                     .padding()
-                    .frame(width: 200.0, height: 200.0)
-            Text("PPG:")
-                .padding()
-            Text("APG:")
-                .padding()
-            Text("RPG:")
-                .padding()
+                VStack(alignment: .leading) {
+                    if let image = model.image {
+                        Image(uiImage: image)
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fill)
+                            .padding()
+                            .frame(width: 200.0, height: 200.0)
+                            .cornerRadius(100)
+                        
+                    } else {
+                        
+                    Image(systemName: "person.circle")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                        .frame(width: 200.0, height: 200.0)
+                    }
+                    Text("PPG:")
+                        .padding()
+                    Text("APG:")
+                        .padding()
+                    Text("RPG:")
+                        .padding()
+                }
+            }
+            .navigationTitle("Stats")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button(action: {
+                        model.isPresentingAlert = true
+                    }, label: {
+                        Image(systemName: "photo.on.rectangle")
+                    })
+                }
+            }
+            .popover(item: $model.presentedView, content: { view in
+                switch view {
+                case .camera:
+                    Camera(image: $model.image)
+                case .photoPicker:
+                    PhotoPicker(image: $model.image)
+                    
+                }
+            })
+            .alert("Image Source", isPresented: $model.isPresentingAlert) {
+                Button(action: {
+                    model.presentedView = .camera
+                }, label: {
+                    Text("Camera")
+                })
+                Button(action: {
+                    model.presentedView = .photoPicker
+                }, label: {
+                    Text("Gallery")
+                })
             }
         }
     }
