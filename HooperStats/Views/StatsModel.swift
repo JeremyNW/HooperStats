b672203a00570639984bac3bdf7691b9aa008643
Zachary Buffington <88993567+Zbcolgrim@users.noreply.github.com>
2022-03-02 09:57:13 -0700

    Zach (#8)
    * Game Averages and Camera access
    
    * profile jpeg save

--- HooperStats/Views/StatsModel.swift
+++ HooperStats/Views/StatsModel.swift
@@ -7,12 +7,42 @@
 
 import Foundation
 import UIKit
-
+import Combine
 
 class StatsViewModel: ObservableObject {
     @Published var isPresentingAlert = false
     @Published var presentedView: PresentedView?
     @Published var image: UIImage?
+    private var imageSubscription: AnyCancellable?
+    private var saveFileURL: URL? {
+        try? FileManager.default.url(
+            for: .documentDirectory,
+               in: .userDomainMask,
+               appropriateFor: nil,
+               create: true
+        )
+            .appendingPathComponent("profile")
+            .appendingPathExtension("jpeg")
+    }
+    func onAppear() {
+        loadImage()
+        autoSaveImage()
+    }
+    func loadImage() {
+        if let saveFileURL = saveFileURL,
+        let data = try? Data(contentsOf: saveFileURL),
+           let image = UIImage(data: data) {
+            self.image = image
+        }
+    }
+    func autoSaveImage() {
+        imageSubscription = $image.sink(receiveValue: { [weak self] image in
+            if let saveFileURL = self?.saveFileURL,
+               let data = self?.image?.jpegData(compressionQuality: 0.8) {
+                try? data.write(to: saveFileURL)
+            }
+        })
+    }
     enum PresentedView: String, Identifiable {
         case camera
         case photoPicker
